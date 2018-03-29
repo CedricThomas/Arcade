b28b5b1ec8d6e0ede184e9022d61927a5c799f2f
Jeremie Bruhwiler <jeremie.bruhwiler@epitech.eu>
2018-03-29 15:05:58 +0200

    [~]api/Color.cpp

--- api/Color.cpp
+++ api/Color.cpp
@@ -8,47 +8,64 @@
 #include "Color.hpp"
 
 Arcade::Color::Color(unsigned char red, unsigned char green, unsigned char blue,
-		     unsigned char alpha) {
-
+		     unsigned char alpha)
+{
+	_red = red;
+	_green = green;
+	_blue = blue;
+	_alpha = alpha;
 }
 
 void Arcade::Color::setColor(unsigned char red, unsigned char green,
-			     unsigned char blue, unsigned char alpha) {
-
+			     unsigned char blue, unsigned char alpha)
+{
+	_red = red;
+	_green = green;
+	_blue = blue;
+	_alpha = alpha;
 }
 
-unsigned char Arcade::Color::getRed() const {
-	return 0;
+unsigned char Arcade::Color::getRed() const
+{
+	return _red;
 }
 
-unsigned char Arcade::Color::getGreen() const {
-	return 0;
+unsigned char Arcade::Color::getGreen() const
+{
+	return _green;
 }
 
-unsigned char Arcade::Color::getBlue() const {
-	return 0;
+unsigned char Arcade::Color::getBlue() const
+{
+	return _blue;
 }
 
-unsigned char Arcade::Color::getAlpha() const {
-	return 0;
+unsigned char Arcade::Color::getAlpha() const
+{
+	return _alpha;
 }
 
-void Arcade::Color::setRed(unsigned char red) {
-
+void Arcade::Color::setRed(unsigned char red)
+{
+	_red = red;
 }
 
-void Arcade::Color::setGreen(unsigned char green) {
-
+void Arcade::Color::setGreen(unsigned char green)
+{
+	_green = green
 }
 
-void Arcade::Color::setBlue(unsigned char blue) {
-
+void Arcade::Color::setBlue(unsigned char blue)
+{
+	_blue = blue
 }
 
-void Arcade::Color::setAlpha(unsigned char alpha) {
-
+void Arcade::Color::setAlpha(unsigned char alpha)
+{
+	_alpha = alpha
 }
 
-Arcade::Color::operator unsigned char *() {
-	return nullptr;
+Arcade::Color::operator unsigned char *()
+{
+	return &_red;
 }
