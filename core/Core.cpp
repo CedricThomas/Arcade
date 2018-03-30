78910587d3b888b3e2af4d0f83918dc940e911ce
Cédric THOMAS <cedric.thomas@epitech.eu>
2018-03-30 13:43:37 +0200

    [~] core

--- core/Core.cpp
+++ core/Core.cpp
@@ -55,7 +55,6 @@ void Arcade::Core::loadLibs(const std::string &directory)
 
 void Arcade::Core::play()
 {
-	std::cout << "play" << std::endl;
 
 	auto box1 = PixelBox(Vect<size_t>(200, 200), Vect<size_t>(100,100), Color(0, 255, 0, 255));
 	box1.putRect(Vect<size_t>(10, 10), Vect<size_t>(10, 10), Color(0, 0, 255, 255));
@@ -64,15 +63,19 @@ void Arcade::Core::play()
 
 	auto text1 = TextBox("Prout", Vect<size_t >(80, 80));
 
-	_lib->getInstance()->openRenderer();
+	for (size_t i = 0; i < _libs.size(); i++) {
+		std::cout << _libs[i] << std::endl;
+		selectGraphByIdx(i);
+		_lib->getInstance()->openRenderer();
 
-	_lib->getInstance()->drawPixelBox(box1);
-	_lib->getInstance()->drawPixelBox(box2);
-	_lib->getInstance()->drawText(text1);
+		_lib->getInstance()->drawPixelBox(box1);
+		_lib->getInstance()->drawPixelBox(box2);
+		_lib->getInstance()->drawText(text1);
 
-	_lib->getInstance()->refreshWindow();
-	getchar();
-	_lib->getInstance()->closeRenderer();
+		_lib->getInstance()->refreshWindow();
+		getchar();
+		_lib->getInstance()->closeRenderer();
+	}
 }
 
 void Arcade::Core::menu()
