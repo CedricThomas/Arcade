fc20c8f7ab40508fa2c81c2f765b7475e0c375f5
Jeremie Bruhwiler <jeremie.bruhwiler@epitech.eu>
2018-04-05 13:46:58 +0200

    [+] Ghost

--- games/pacman/Ghost.cpp
+++ games/pacman/Ghost.cpp
@@ -6,3 +6,42 @@
 */
 
 #include "Ghost.hpp"
+
+void Arcade::Ghost::init()
+{
+
+}
+
+void Arcade::Ghost::changeDir(const Arcade::Vect<int> &dir)
+{
+	auto newX = _pos.getX() + dir.getX();
+	auto newY = _pos.getY() + dir.getY();
+	if (_board[newY][newX] != WALL) {
+		_dir = dir;
+	}
+}
+
+void Arcade::Ghost::kill()
+{
+
+}
+
+void Arcade::Ghost::update(Player &pacman)
+{
+	//if (pacman)
+}
+
+void Arcade::Ghost::draw(Arcade::PixelBox &pixebox,
+			 const Arcade::Vect<size_t> &size)
+{
+
+}
+
+Arcade::Ghost::Ghost(const Vect<int> &initPos,
+		     std::vector<std::vector<int>> &board) :
+	_initPos(initPos),
+	_dir({0, -1}),
+	_pos(initPos),
+	_last(),
+	_board(board)
+{}
--- games/pacman/Ghost.hpp
+++ games/pacman/Ghost.hpp
@@ -8,10 +8,29 @@
 #ifndef ARCADE_GHOST_HPP
 	#define ARCADE_GHOST_HPP
 
+	#include <vector>
+	#include "Vect.hpp"
+	#include "PixelBox.hpp"
+	#include "Player.hpp"
 
 namespace Arcade {
 
 	class Ghost {
+		void init();
+
+		public:
+		Ghost(const Vect<int> &_initPos, std::vector<std::vector<int>> &board);
+		void changeDir(const Vect<int> &dir);
+		void kill();
+		void update(Player &pacman);
+		void draw(PixelBox &pixebox, const Vect<size_t> &size);
+
+		private:
+		Vect<int> _initPos;
+		Vect<int> _dir;
+		Vect<int> _pos;
+		std::chrono::high_resolution_clock::time_point _last;
+		std::vector<std::vector<int>> _board;
 
 	};
 
